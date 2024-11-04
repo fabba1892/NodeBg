f24eb29645b4550be3dd80bfba482eb8f9717fa5
Fabian <Fabian.vanwyk@gmail.com>
2024-11-04 22:35:01 +0200

    add post requst

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const port = 8000;
 const app = express();
+//
 app.get("/", (req, res) => res.send("Awe from Parow"));
+//
+app.post('/addemployee', function(req, res){
+  let empName = req.body.empName  ;
+  let empPass = req.body.empPass;
+  res.end(`Post success ` + empName);  
 
-app.listen(port, function () {
-  console.log("Listening" + port);
-});
+})
+//
+app.listen(port, () => console.log("Listening " + port));
+
