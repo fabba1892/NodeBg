94a9cf62372c6425e888e908ef423758e43b2d25
Fabian <Fabian.vanwyk@gmail.com>
2024-11-06 11:38:52 +0200

    get and post functions is worlking

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,5 +3,16 @@ exports.getdefault = function(req, res) {
 };
 
 exports.aboutUs = function (req, res) {
-      res.send("You are on the about us page");
+      res.send("You are on the about us page 🔱");
+};
+
+exports.addemployee = function (req, res) {
+    //res.send("Employee route page🗼")    ;
+    let empName = req.body.empName;
+    let empPass  = req.body.empPass;
+    res.end(`Post success, you sent ${empName} and ${empPass}, thanks👍🏽 `)
+};
+
+exports.getemployees = function (req, res) {
+    res.send("Get Docs page 📂");
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,7 @@
 const controller = require('../controllers/controller');
 module.exports = function (router) {
     router.get("/", controller.getdefault);
-    router.post('/addemployee', function(req, res){
-        let empName = req.body.empName;
-        let empPass = req.body.empPass;
-        res.end(`Post success, you sent ${empName} and ${empPass}, thanks`);  
-    });
+    router.post('/addemployee', controller.addemployee);
     router.get('/aboutus', controller.aboutUs);
+    router.get('/getemployees', controller.getemployees);
 }
