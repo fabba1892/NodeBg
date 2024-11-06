72f7498817a2b2a7e259a630156339d685f3260f
Fabian <Fabian.vanwyk@gmail.com>
2024-11-06 10:13:53 +0200

    register router with the app use as middleware

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const port = 8000;
 const app = express();
-app.use = (express.json());
+const router = express.Router();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 //
-app.get("/", (req, res) => res.send("Awe from Parow"));
+router.get("/", (req, res) => res.send("Awe from Parow"));
 //
-app.post('/addemployee', function(req, res){
-  let empName = req.body.empName  ;
+router.post('/addemployee', function(req, res){
+  let empName = req.body.empName;
   let empPass = req.body.empPass;
-  res.end(`Post success ` + empName);  
+  res.end(`Post success ` + empName + ` password ` + empPass);  
+});
 
-})
+router.get('/aboutus', function(req, res) {
+  res.send("You are on the about us page");
+});
+
+app.use('/', router);
 //
 app.listen(port, () => console.log("Listening " + port));
 
