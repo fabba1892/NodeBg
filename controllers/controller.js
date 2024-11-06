beb48330a4a0dc8cf761cece74a6d459ada20eca
Fabian <Fabian.vanwyk@gmail.com>
2024-11-06 11:22:53 +0200

    now navigate to diff pages work

---
+++ controllers/controller.js
@@ -0,0 +1,7 @@
+exports.getdefault = function(req, res) {
+    res.send("You are the root route. Awe LucaSkywlkr🤖");
+};
+
+exports.aboutUs = function (req, res) {
+      res.send("You are on the about us page");
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,10 @@
+const controller = require('../controllers/controller');
 module.exports = function (router) {
-    router.get("/", (req, res) => res.send("Awe from Parow"));
+    router.get("/", controller.getdefault);
     router.post('/addemployee', function(req, res){
         let empName = req.body.empName;
         let empPass = req.body.empPass;
         res.end(`Post success, you sent ${empName} and ${empPass}, thanks`);  
-      });
-      
-      router.get('/aboutus', function(req, res) {
-        res.send("You are on the about us page");
-      });
+    });
+    router.get('/aboutus', controller.aboutUs);
 }
